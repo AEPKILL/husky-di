dad0352979be8325ae4ddc20231a8f4848e86076
侯含 <houhan664@pingan.com.cn>
2021-12-21 15:39:38 +0800

    chore: export type

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,21 @@
  * @created 2021-10-11 15:49:22
  */
 
-export { ClassProvider } from './providers/class.provider';
-export { FactoryProvider } from './providers/factory.provider';
-export { ValueProvider } from './providers/value.provider';
+export {
+  ClassProvider,
+  ClassProviderOptions,
+} from './providers/class.provider';
+export {
+  FactoryProvider,
+  FactoryProviderOptions,
+} from './providers/factory.provider';
+export {
+  ValueProvider,
+  ValueProviderOptions,
+} from './providers/value.provider';
 
-export { tagged as Tagged } from './decorators/tagged.decorator';
-export { inject as Inject } from './decorators/inject.decorator';
+export { tagged } from './decorators/tagged.decorator';
+export { inject } from './decorators/inject.decorator';
 export { compilerMetadata as CompilerMetadata } from './decorators/compiler-metadata.decorator';
 
 export { LifecycleEnum } from './enums/lifecycle.enum';
@@ -20,6 +29,12 @@ export { Ref } from './types/ref.type';
 
 export { createContainer } from './factory/create-container.factory';
 export { createServiceIdentifier } from './factory/create-service-identifier.factory';
+export { ServiceIdentifier } from './types/service-identifier.type';
+
+export {
+  IContainer,
+  ResolveReturnType,
+} from './interfaces/container.interface';
 
 /**
  * # 代表 container
