55d30014c084f128b02dca9f86e954d93638c10a
AEPKILL <yuanstop@gmail.com>
2023-06-07 10:38:30 +0800

    feat: add "createServiceDecorator"

--- src/classes/service-identifier-manager.ts
+++ src/classes/service-identifier-manager.ts
@@ -4,9 +4,12 @@
  * @created 2021-10-11 17:25:47
  */
 
+import { inject } from "@/decorators/inject.decorator";
 import { getServiceIdentifierName } from "@/utils/service-identifier.utils";
 
 import type { ServiceIdentifier } from "@/types/service-identifier.type";
+import type { ServiceDecorator } from "@/types/service-decorator.type";
+import type { ResolveOptions } from "@/interfaces/container.interface";
 
 export class ServiceIdentifierManager {
   private _serviceIdentifiers: Set<ServiceIdentifier<any>>;
@@ -33,5 +36,19 @@ export class ServiceIdentifierManager {
     new ServiceIdentifierManager();
   static createServiceIdentifier<T>(id: string | symbol): ServiceIdentifier<T> {
     return this.defaultServiceIdentifierManager.createServiceIdentifier(id);
+  }
+
+  static createServiceDecorator<T>(
+    serviceIdentifier: ServiceIdentifier<T>
+  ): ServiceDecorator<T> {
+    const decorator = function (
+      options?: ResolveOptions<T>
+    ): ParameterDecorator {
+      return inject(serviceIdentifier, options);
+    } as ServiceDecorator<T>;
+
+    decorator.serviceIdentifier = serviceIdentifier;
+
+    return decorator;
   }
 }
