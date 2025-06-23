a1b63d03f62b3904242e86ade157dfa2c8310d01
AEPKILL <yuanstop@gmail.com>
2025-06-23 22:38:50 +0800

    refactor: v2 init

--- jest.setup.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @overview
- * @author AEPKILL
- * @created 2023-05-26 13:39:40
- */
-
-import "reflect-metadata";
--- src/interfaces/disposable.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @overview
- * @author AEPKILL
- * @created 2022-10-13 17:36:35
- */
-
-export interface IDisposable {
-  dispose(): void;
-}
