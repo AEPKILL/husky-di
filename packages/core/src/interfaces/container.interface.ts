0c20335d9404cf1a741a0d7f329d3ba521f3a2e7
AEPKILL <yuanstop@gmail.com>
2025-06-26 11:45:54 +0800

    refactor: enhance IContainer interface and add new ResolveOptions types for improved type safety

--- packages/core/src/interfaces/container.interface.ts
+++ packages/core/src/interfaces/container.interface.ts
@@ -7,13 +7,8 @@
 import type { IUnique } from "@/interfaces/unique.interface";
 import type { Ref } from "@/types/ref.type";
 
-export interface IContainer extends IUnique {
-	readonly name: string;
-}
-
 /**
- * 解析选项接口
- * 提供严格的类型约束：
+ * ResolveOptions:
  * - 只有当 optional 为 true 时才允许传入 defaultValue
  * - 当 multiple 为 true 时，defaultValue 必须是 T[]
  * - 当 multiple 为 false 时，defaultValue 必须是 T
@@ -21,27 +16,76 @@ export interface IContainer extends IUnique {
 export type ResolveOptions<T> = {
 	dynamic?: boolean;
 	ref?: boolean;
-} & (
+} & ( // 不需要多个实例，且不是可选的，不能有 defaultValue
 	| {
-			/** 不需要多个实例，且不是可选的 */
 			multiple?: false;
 			optional?: false;
+			defaultValue?: never;
 	  }
+	// 不需要多个实例，但是可选，可以有单个 defaultValue
 	| {
-			/** 不需要多个实例，但是可选的，可以提供单个默认值 */
 			multiple?: false;
 			optional: true;
 			defaultValue?: T;
 	  }
+	// 需要多个实例，但不是可选的，不能有 defaultValue
 	| {
-			/** 需要多个实例，但不是可选的 */
 			multiple: true;
 			optional?: false;
+			defaultValue?: never;
 	  }
+	// 需要多个实例，且是可选的，可以有数组 defaultValue
 	| {
-			/** 需要多个实例，且是可选的，可以提供数组默认值 */
 			multiple: true;
 			optional: true;
 			defaultValue?: T[];
 	  }
 );
+
+/**
+ * 判断是否为数组类型
+ * @param T 实例类型
+ * @param O 选项类型
+ */
+type ResolveArrayType<T, O extends ResolveOptions<T>> = O extends {
+	multiple: true;
+}
+	? T[]
+	: T;
+
+/**
+ * 判断是否为 optional 且无 defaultValue
+ * @param T 实例类型
+ * @param O 选项类型
+ */
+type ResolveOptionalType<T, O extends ResolveOptions<T>> = O extends {
+	optional: true;
+	defaultValue?: undefined;
+}
+	? ResolveArrayType<T, O> | undefined
+	: ResolveArrayType<T, O>;
+
+/**
+ * 判断是否需要 Ref 包裹
+ * @param T 实例类型
+ * @param O 选项类型
+ */
+type ResolveRefType<T, O extends ResolveOptions<any>> = O extends
+	| { ref: true }
+	| { dynamic: true }
+	? Ref<T>
+	: T;
+
+/**
+ * 根据 ResolveOptions 推导返回类型
+ * @param T 实例类型
+ * @param O 选项类型
+ */
+export type ResolveInstance<T, O extends ResolveOptions<T>> = ResolveRefType<
+	ResolveOptionalType<T, O>,
+	O
+>;
+
+export interface IContainer extends IUnique {
+	readonly name: string;
+}
