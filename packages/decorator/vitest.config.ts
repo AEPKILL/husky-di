96208659ad6aafafa2aa20e1a27066405498c232
AEPKILL <yuanstop@gmail.com>
2025-08-07 23:53:48 +0800

    chore: update Vitest configuration to set node environment and add path alias resolution

--- packages/decorator/vitest.config.ts
+++ packages/decorator/vitest.config.ts
@@ -1,6 +1,14 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	// Configure Vitest (https://vitest.dev/config/)
-	test: {},
+	test: {
+		environment: "node",
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
